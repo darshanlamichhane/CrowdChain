a2904cc43a261d18cdea5b2e8f6b61c246c6acf2
Santosh Yadav <ymanoutube@gmail.com>
2023-11-10 22:31:42 +0545

    Folder structure completed and routing is done!

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+      This is the navbar
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='text-blue-600'>
+      This is the sidebar
+    </div>
+  )
+}
+
+export default Sidebar
